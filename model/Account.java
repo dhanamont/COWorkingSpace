c474ce72f4d6d8d687bf9eafab13ef0c729da194
Dhanamon <dhanamont@users.noreply.github.com>
2017-10-20 23:20:19 +0700

    Update Account.java

--- model/Account.java
+++ model/Account.java
@@ -15,10 +15,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-/**
- *
- * @author test
- */
+
 public class Account {
     Connection conn;
 
