f5fdded880a231b32e9f7b5f46a9ec0e1c9c00ce
Dhanamon <dhanamont@users.noreply.github.com>
2017-12-10 19:05:42 +0700

    Create Table.java

---
+++ model/Table.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author test
+ */
+public class Table {
+    Connection con;
+    public Table() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        java.util.Properties prop = new java.util.Properties();
+        prop.put("charSet","UTF-8");
+        this.con = DriverManager.getConnection("jdbc:mysql://ihost.it.kmitl.ac.th:3306/it58070122_se?zeroDateTimeBehavior=convertToNull&characterEncoding=utf-8", 
+                "it58070122_se",
+                "chFKW9lGV");
+    }
+    
+    String Table_ID;
+    
+    public String createTable_ID() {
+        try {
+            Statement stmt = con.createStatement();
+            String numSpa = "SELECT COUNT(Table_ID)+1 from `Table`";
+            ResultSet numSpa1 = stmt.executeQuery(numSpa);
+            numSpa1.next();
+            Table_ID = numSpa1.getString("count(Table_ID)+1");
+
+        } catch (SQLException ex) {
+        }
+        return Table_ID;
+    }
+    
+    public void insertTable(String TableID, int NumofPeople, String Room_ID) {
+        try {
+            Statement stmt = con.createStatement();
+            String sql = "Insert into `Table` values('" + TableID + "', '" + NumofPeople + "', '" + Room_ID +"';";
+            stmt.executeUpdate(sql);
+        } catch (SQLException ex) {
+        }
+    }
+    
+    public String getTable_ID(String Room_ID) {
+        try {
+            Statement stmt = con.createStatement();
+            String table = "SELECT Table_ID FROM `Table` JOIN Room_ID) = '" + Room_ID + "';";
+            ResultSet rs1 = stmt.executeQuery(table);
+            rs1.next();
+            Table_ID = rs1.getString("Type_ID");
+        } catch (SQLException ex) {
+        }
+        return Table_ID;
+    }
+}
