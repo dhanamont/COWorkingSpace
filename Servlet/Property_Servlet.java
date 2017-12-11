acec84b401df9dcc834d1e1cab9ccdf0cf9e0e53
Lalita Sareeroop <58070122@kmitl.ac.th>
2017-12-12 01:48:30 +0700

    Update Property_Servlet.java

--- Servlet/Property_Servlet.java
+++ Servlet/Property_Servlet.java
@@ -10,6 +10,7 @@ import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -18,9 +19,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import model.Space;
+import model.Entrepreneur;
 import model.Room;
+import model.Space;
 import model.Table;
+import model.Type_Space;
+//import model.Room;
+//import model.Table;
 
 /**
  *
@@ -39,47 +44,65 @@ public class Property_Servlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, ParseException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
 
             HttpSession session = request.getSession();
-
-            String Space_ID = request.getParameter("Space_ID");
+            String Space_ID = request.getParameter("id");
             request.setAttribute("Space_ID", Space_ID);
 
-            String Type_ID = request.getParameter("Type_ID");
+            Room room = new Room();
+            Space space = new Space();
+            Type_Space type = new Type_Space();
+            Entrepreneur ent = new Entrepreneur();
+            
+            
+            String Type_ID = type.getType_ID(Space_ID);
             request.setAttribute("Type_ID", Type_ID);
             
             ArrayList<String> Room_ID = new ArrayList<String>();
             ArrayList<Float> Price = new ArrayList<Float>();
             ArrayList<String> Room_Name = new ArrayList<String>();
+            ArrayList<String> Service_Name = new ArrayList<String>();
+            ArrayList<String> Picture_room = new ArrayList<String>();
             
-            Room room = new Room();
-            Space space = new Space();
+            room.getRoomList(Room_ID,Room_Name,Price,Picture_room,Type_ID);
+            space.getService_Name(Service_Name, Space_ID);
+            String User_ID = space.getUser_ID(Space_ID);
             
-            room.getRoomList(Room_ID,Room_Name,Price,Type_ID);
-            room.getListProp(Type_ID);
             
-            ArrayList<String> openDate = space.getOpenDate(Space_ID);
+            //วันเวลา
+            //ArrayList<String> openDate = space.getOpenDate(Space_ID);
             ArrayList<String> openTime = space.getOpenTime(Space_ID);
 
-
+            //ส่งค่าไป jsp
             request.setAttribute("Space_Name", space.getSpace_Name(Space_ID));
             request.setAttribute("Address", space.getAddress(Space_ID));
             request.setAttribute("Place", space.getPlace(Space_ID));
-            request.setAttribute("Picture_poster", space.getPicture_Poster(Space_ID));
             request.setAttribute("Picture_cover", space.getPicture_Cover(Space_ID));
             request.setAttribute("Start_Time", space.getStart_Time(Space_ID));
             request.setAttribute("End_Time", space.getEnd_Time(Space_ID));
+            request.setAttribute("Map", space.getMap(Space_ID));
+            request.setAttribute("Prototype", type.getPrototype(Type_ID));
             request.setAttribute("Description", space.getDescription(Space_ID));
-            request.setAttribute("Date", openDate);
+            //request.setAttribute("Date", openDate);
             request.setAttribute("Time", openTime);
             request.setAttribute("Price", Price);
             request.setAttribute("Room_Name", Room_Name);
+            request.setAttribute("Service_Name", Service_Name);
+            request.setAttribute("Picture_room", Picture_room);
+            
+            //ส่ง list RoomID TableID ไป jsp
+            
+            //------------------ผปก.
+            request.setAttribute("Contact_name", ent.getContact_name(User_ID));
+            request.setAttribute("Email", ent.getEmail(User_ID));
+            request.setAttribute("Company_name", ent.getCompany_name(User_ID));
+            request.setAttribute("Phone", ent.getPhone(User_ID));
 
             request.getRequestDispatcher("Property.jsp").forward(request, response);
-        } catch (SQLException | ClassNotFoundException | ParseException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(Property_Servlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -96,7 +119,15 @@ public class Property_Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            try {
+                processRequest(request, response);
+            } catch (ParseException ex) {
+                Logger.getLogger(Property_Servlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Property_Servlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -110,7 +141,15 @@ public class Property_Servlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            try {
+                processRequest(request, response);
+            } catch (ParseException ex) {
+                Logger.getLogger(Property_Servlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Property_Servlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
