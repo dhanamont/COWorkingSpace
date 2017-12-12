087473212650f246366638a493acf5333aa3fb28
Dhanamon <dhanamont@users.noreply.github.com>
2017-12-13 00:10:41 +0700

    Update Date.java

--- model/Date.java
+++ model/Date.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 import java.sql.Connection;
@@ -11,8 +10,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Locale.Category;
 
 /**
  *
